f26262b9008ad9677dcb37ce9d30f5af240a8952
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-10-19 19:00:59 +0300

    file

---
+++ #src/js/index/index.js
@@ -0,0 +1,27 @@
+//! Custom ScrollBar
+
+$(function() {  
+    $("body").niceScroll({ 
+        cursorcolor: "#ADD4A9",
+        cursorborder: ".5px solid box-shadow: 2px 4px 13px 2px rgba(34, 60, 80, 0.2);", 
+        zindex: 100000,
+    });
+});
+
+//! -----------------------------------------------------------------------------
+
+
+
+//! Header slider 
+
+
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+        nextEl: ".ind-headers__slider .swiper-controls .swiper-button-next",
+        prevEl: ".ind-headers__slider .swiper-controls .swiper-button-prev",
+    },
+    keyboard: true,
+});
+
+
+//! ------------------------------------------------------------------------------
---
+++ readme.md
@@ -0,0 +1,1 @@
+# Index: [click me](https://kah3vich.github.io/Islamic/dist/index.html)
