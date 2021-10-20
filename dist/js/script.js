cbab4a879a525553b9400b7812d2a0bf8f7ee36a
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-10-20 15:13:29 +0300

    file

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,12 @@
 //! Custom ScrollBar
 
-$(function() {  
-    $("body").niceScroll({ 
-        cursorcolor: "#ADD4A9",
-        cursorborder: ".5px solid box-shadow: 2px 4px 13px 2px rgba(34, 60, 80, 0.2);", 
-        zindex: 100000,
-    });
-});
+// $(function() {  
+//     $("body").niceScroll({ 
+//         cursorcolor: "#ADD4A9",
+//         cursorborder: ".5px solid box-shadow: 2px 4px 13px 2px rgba(34, 60, 80, 0.2);", 
+//         zindex: 100000,
+//     });
+// });
 
 //! -----------------------------------------------------------------------------
 
@@ -42,3 +42,52 @@ var swiper = new Swiper(".mySwiper", {
 //! ------------------------------------------------------------------------------
 
 
+
+var indexBusinessSlider = new Swiper(".indexBusinessSlider", {
+    navigation: {
+        nextEl: ".ind-business__slider .swiper-controls .swiper-button-next",
+        prevEl: ".ind-business__slider .swiper-controls .swiper-button-prev",
+    },
+    slidesPerView: 3,
+    spaceBetweenSlides: 0,
+    keyboard: true,
+    reakpoints: {
+        0: {
+            slidesPerView: 3,
+            spaceBetweenSlides: 0
+        },
+    }
+});
+
+
+var indexBookSlider = new Swiper(".indexBookSlider", {
+    navigation: {
+        nextEl: ".ind-book__slider .swiper-controls .swiper-button-next",
+        prevEl: ".ind-book__slider .swiper-controls .swiper-button-prev",
+    },
+    slidesPerView: 4,
+    spaceBetweenSlides: 30,
+    keyboard: true,
+    reakpoints: {
+        0: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 30
+        },
+    }
+});
+
+var indexPartnerSlider = new Swiper(".indexPartnerSlider", {
+    navigation: {
+        nextEl: ".ind-partner__slider .swiper-controls .swiper-button-next",
+        prevEl: ".ind-partner__slider .swiper-controls .swiper-button-prev",
+    },
+    slidesPerView: 4,
+    spaceBetweenSlides: 30,
+    keyboard: true,
+    reakpoints: {
+        0: {
+            slidesPerView: 4,
+            spaceBetweenSlides: 30
+        },
+    }
+});
