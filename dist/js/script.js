963dbcb23c3a770ec1a890403b2f23af93fbae94
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-11-01 12:59:18 +0300

    file

--- dist/js/script.js
+++ dist/js/script.js
@@ -491,6 +491,46 @@ $('#catalogButtonClose').on('click', function() {
     $('#catalogContentAdd').addClass('display-n')
     $('#catalogButtonAdd').removeClass('display-n')
 });
+
+
+//! Открытие фильтра 
+
+$('.catl-headers__nav-text-filter-block').on('click', function() {
+    $('.catl-headers__nav-text-filter-wrapper').toggleClass('display-n');
+});
+
+
+//! -------------------------------------------------------------------------------
+
+
+//! Передача значение из input range
+
+$('#marchRange-1').on('input',function () {
+    $('.catl-headers__nav-text-filter-range-value-1').empty().append($(this).val());
+});
+$('#marchRange-2').on('input',function () {
+    $('.catl-headers__nav-text-filter-range-value-2').empty().append($(this).val());
+});
+
+//! -------------------------------------------------------------------------------
+
+
+//! Сброс всех input в фильтре 
+
+const marchRange_1 = 1680;
+const marchRange_2 = 16090;
+$('.catl-headers__nav-text-filter-discard').on('click', function() {
+    $('#checkbox_1').prop('checked', false);
+    $('#checkbox_2').prop('checked', false);
+    $('#checkbox_3').prop('checked', false);
+    $('#checkbox_4').prop('checked', false);
+    $('#checkbox_5').prop('checked', false);
+    $('#checkbox_6').prop('checked', true);
+    $('#marchRange-1').val(marchRange_1);
+    $('.catl-headers__nav-text-filter-range-value-1').empty().append($('#marchRange-1').val());
+    $('#marchRange-2').val(marchRange_2);
+    $('.catl-headers__nav-text-filter-range-value-2').empty().append($('#marchRange-2').val());
+});
 $('#detailAddTabsButton-1').on('click', function() {
     $(this).addClass('det-content__add-tabs-item-active')
     $('#detailAddTabsButton-2').removeClass('det-content__add-tabs-item-active')
