2727655c32d6d2fbacf2c320c4610d47c2507a05
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-11-30 20:26:04 +0300

    file

--- dist/js/script.js
+++ dist/js/script.js
@@ -667,3 +667,12 @@ $('#detailAddTabsButton-3').on('click', function() {
     $('#detailAddTabsButton-2').removeClass('det-content__add-tabs-item-active')
     $('#detailAddTabsButton-1').removeClass('det-content__add-tabs-item-active')
 });
+
+
+$('.det-message__block-btn').on('click', function() {
+    if ($('.det-message__block-textarea').val() == '') {
+        $('.det-message__block-textarea').css('border', '1px solid #FF0000')
+    } else {
+        $('.det-message__block-textarea').css('border', '1px solid #e1e1e1')
+    }
+});
